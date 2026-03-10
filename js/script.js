e1638a1d08fa7067f48171edb087df799113f583
scontreraslopez <s.contreraslopez@edu.gva.es>
2026-03-10 18:38:58 +0100

    with eras json

--- js/script.js
+++ js/script.js
@@ -56,6 +56,105 @@ const eras = [
      ]
   },
   //Completa tu el resto
+  {
+    era: "Era Antigua",
+    imagen: "assets/galleta-antigua.jpg",
+    citas: [
+      { tecnologia: "Alfabeto",
+        texto: "Las palabras tienen el poder de destruir y sanar. Cuando las palabras son verdaderas y amables, pueden cambiar nuestro mundo.",
+        autor: "Buda" },
+      { tecnologia: "Brújula",
+        texto: "Los hombres más sabios siguen su propia dirección.",
+        autor: "Eurípides" },
+      { tecnologia: "Construcción",
+        texto: "Y en el pedestal aparecen estas palabras: 'Mi nombre es Ozymandias, rey de reyes: ¡Contemplad mis obras, oh Poderosos, y desesperad!'. No queda nada más.",
+        autor: "Percy Bysshe Shelley" },
+      { tecnologia: "Moneda",
+        texto: "Todo vale lo que su comprador esté dispuesto a pagar por ello.",
+        autor: "Publilio Siro" },
+      { tecnologia: "Literatura",
+        texto: "Algunos libros son para ser probados, otros para ser tragados, y algunos para ser masticados y digeridos.",
+        autor: "Sir Francis Bacon" },
+      { tecnologia: "Matemáticas",
+        texto: "Si en otras ciencias llegamos a la certeza sin duda y a la verdad sin error, nos incumbe poner los cimientos del conocimiento en las matemáticas.",
+        autor: "Roger Bacon" }
+    ]
+  },
+  { era: "Era Medieval",
+    imagen: "assets/galleta-medieval.jpg",
+    citas: [
+      { tecnologia: "Banca",
+      texto: "Los establecimientos bancarios son más peligrosos que los ejércitos permanentes.",
+      autor: "Thomas Jefferson" },
+      { tecnologia: "Ingeniería",
+        texto: "Un diseñador sabe que ha alcanzado la perfección no cuando ya no queda nada que añadir, sino cuando ya no queda nada que quitar.",
+        autor: "Antoine de Saint-Exupéry" },
+      { tecnologia: "Óptica",
+        texto: "Uno no descubre nuevas tierras sin perder de vista la orilla.",
+        autor: "André Gide" },
+      { tecnologia: "Filosofía",
+        texto: "He ganado esto por la filosofía: que hago sin que me lo manden lo que otros hacen solo por miedo a la ley.",
+        autor: "Aristóteles" }
+    ]
+  },
+  {
+    era: "Era Renacentista",
+    imagen: "assets/galleta-renacentista.jpg",
+    citas: [
+      { tecnologia: "Astronomía",
+      texto: "La astronomía obliga al alma a mirar hacia arriba y nos lleva de este mundo a otro.",
+      autor: "Platón" },
+      { tecnologia: "Pólvora",
+      texto: "Puedes conseguir más de lo que quieres con una palabra amable y un arma que solo con una palabra amable.",
+      autor: "Al Capone" },
+      { tecnologia: "Nacionalismo",
+      texto: "Un hombre no se hace matar por medio penique al día o por una pequeña distinción. Debes hablarle al alma para electrificarlo.",
+      autor: "Napoleón Bonaparte" },
+      { tecnologia: "Imprenta",
+      texto: "Lo que la pólvora hizo por la guerra, la imprenta lo ha hecho por la mente.",
+      autor: "Wendell Phillips" }
+    ]
+  },
+  { era: "Era Industrial",
+    imagen: "assets/galleta-industrial.jpg",
+    citas: [
+      { tecnologia: "Biología",
+      texto: "No es la más fuerte de las especies la que sobrevive, sino la que mejor responde al cambio.",
+      autor: "Charles Darwin" },
+      { tecnologia: "Comunismo",
+      texto: "Cuando doy comida a los pobres, me llaman santo. Cuando pregunto por qué los pobres no tienen comida, me llaman comunista.",
+      autor: "Dom Helder Camara" },
+      { tecnologia: "Fisión",
+      texto: "Si el resplandor de mil soles estallara de una vez en el cielo, sería como el esplendor del Todopoderoso... Me he convertido en la Muerte, el Destructor de Mundos.",
+      autor: "J. Robert Oppenheimer, citando el \"Bhagavad Gita\"" },
+      { tecnologia: "Medicina",
+      texto: "En cuanto a las enfermedades, adquiere el hábito de dos cosas: ayudar, o al menos, no hacer daño.",
+      autor: "Hipócrates" }
+    ]
+  },
+  { era: "Era Industrial",
+    imagen: "assets/galleta-industrial.jpg",
+    citas: [
+      { tecnologia: "Ordenadores",
+      texto: "Nunca confíes en un ordenador que no puedas tirar por la ventana.",
+      autor: "Steve Wozniak" },
+      { tecnologia: "Ecología",
+      texto: "No heredamos la tierra de nuestros antepasados, la tomamos prestada de nuestros hijos.",
+      autor: "Canción nativa americana" },
+      { tecnologia: "Vuelo",
+      texto: "Una vez que hayas probado el vuelo, caminarás por la tierra con los ojos mirando al cielo, porque allí has estado y allí anhelarás volver.",
+      autor: "Leonardo Da Vinci" },
+      { tecnologia: "Radio",
+      texto: "Entonces una hermosa mañana ella sintoniza una emisora de Nueva York. Sabes que su vida fue salvada por el Rock 'n' Roll.",
+      autor: "The Velvet Underground, \"Rock And Roll\"" },
+      { tecnologia: "Robótica",
+      texto: "El verdadero problema no es si las máquinas piensan, sino si los hombres lo hacen.",
+      autor: "B.F. Skinner" },
+      { tecnologia: "Láser",
+      texto: "Nada viaja más rápido que la velocidad de la luz, con la posible excepción de las malas noticias, que obedecen a sus propias leyes especiales.",
+      autor: "Douglas Adams" }
+    ]
+  }
 ];
 
 
